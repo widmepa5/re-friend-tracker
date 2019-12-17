51e4ca18a8ea18a5e79763b39ca349fb04ec3761
widmepa5 <58996422+widmepa5@users.noreply.github.com>
2019-12-17 22:55:55 +0100

    Update guimodel.ts

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Patricks Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
